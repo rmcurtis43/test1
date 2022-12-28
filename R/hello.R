56d6199bc58fce27ce961061d7c9309deefae738
rmcurtis43 <rmcurtis43@yahoo.com>
2022-12-27 18:42:30 -0600

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' @title Print Hello, world!
+#'
+#' @return printed message 'Hello, world!'
+#' @export
+#'
+#' @examples
+#' hello()
+#'
+#'
+hello <- function() {
+  print("Hello, world!")
+}
---
+++ tests/testthat/test-hello.R
@@ -0,0 +1,3 @@
+test_that("hello world works", {
+  expect_equal(hello(), "Hello, world!")
+})
